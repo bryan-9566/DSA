389be6e6d83153636c8082118c20df0cecfa2b50
Bryan V <bryan956627@gmail.com>
2026-04-11 08:51:40 +0530

    commit
    Signed-off-by:Bryan Selvam V <bryan956627@gmail.com>

--- STRIVER SHEET SORTING/src/selection.java
+++ STRIVER SHEET SORTING/src/selection.java
@@ -1,3 +1,23 @@
+import java.util.*;
 public class selection {
-
+public static void main(String[] args)
+{
+    int[] a={7,1,3,500,200,100,0};
+    int n=a.length;
+    for(int i=0;i<n-1;i++)
+    {
+        int min=i;
+        for(int j=i+1;j<n;j++)
+        {
+            if(a[j]<a[min])
+            {
+                min=j;
+            }
+        }
+        int temp=a[i];
+        a[i]=a[min];
+        a[min]=temp;
+    }
+    System.out.print(Arrays.toString(a));
+}
 }
