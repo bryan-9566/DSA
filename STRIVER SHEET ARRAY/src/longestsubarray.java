bdab247c63d42046ce46425bbb9cf7deaa976671
Bryan V <bryan956627@gmail.com>
2026-04-17 13:24:52 +0530

    commit
    Signed-off-by:Bryan Selvam V <bryan956627@gmail.com>

---
+++ STRIVER SHEET ARRAY/src/longestsubarray.java
@@ -0,0 +1,30 @@
+public class longestsubarray {
+    public static void main(String[] args)
+    {
+        int[] a = {10, 5, 2, 7, 1, 9};
+        int left=0,right=0;
+        int sum=a[0],len=0,max=0,k=15;
+        while(right<a.length)
+        {
+            if(left<=right && sum>k)
+            {
+                sum-=a[left];
+                left++;
+            }
+            if(sum==k)
+            {
+                len=right-left+1;
+                if(len>max)
+                {
+                    max=len;
+                }
+            }
+            right++;
+            if(right<a.length)
+            {
+                sum+=a[right];
+            }
+        }
+        System.out.print(max);
+    }
+}
