808754292e837d3aaec72d7e29177df690c58294
Bryan V <bryan956627@gmail.com>
2026-05-13 10:15:41 +0530

    commit
    Signed-off-by:Bryan Selvam V <bryan956627@gmail.com>

---
+++ STRIVER SHEET ARRAY/src/mergetwosorted.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class mergetwosorted {
+    public static void main(String[] args)
+    {
+        int[] a1={1,3,5,7};
+        int[] a2={0,2,6,8,9};
+        int l=a1.length-1;
+        int r=0;
+        while(l>=0 && r<a2.length)
+        {
+            if(a1[l]>a2[r])
+            {
+                swap(a1,a2,l,r);
+                l--;
+                r++;
+            }
+            else {
+                break;
+            }
+        }
+        Arrays.sort(a1);
+        Arrays.sort(a2);
+        System.out.print(Arrays.toString(a1));
+        System.out.print(Arrays.toString(a2));
+    }
+    public static void swap(int[] a,int[] b,int i,int j)
+    {
+        int temp=a[i];
+        a[i]=b[j];
+        b[j]=temp;
+    }
+}
